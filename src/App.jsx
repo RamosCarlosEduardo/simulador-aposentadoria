7c518afb2239a8cfca93b321247d458c5b06ad74
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2023-04-30 04:28:29 -0300

    inserting dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import { Field, Form, Formik } from 'formik'
 import {fv} from './utils/futureValue'
 import {realTax} from './utils/realTax'
 import {formataBRL} from './utils/formatCurrency'
+import {MdOutlineLightMode, MdModeNight} from 'react-icons/md'
+import {RxDividerVertical} from 'react-icons/rx'
 
-// Idade Atual
-// Idade que deseja se aposentar
-// Aporte inicial
-// Aporte recorrente
-// Quanto deseja receber após se aposentar
-// Rentabilidade anual
-// Inflação anual
 
 function App() {
   const [calculated, setCalculated] = useState(false)
   const [fvCalculated, setFvCalculated] = useState()
+  const [data, setData] = useState({})
+  const divRoot = useRef(null)
+
+  function switchTheme () {divRoot.current.classList.toggle('dark')}
+
   return (
-    <div className="grid place-content-center h-screen">
+    <div className="grid place-content-center h-screen bg-zinc-500" ref={divRoot}>
+    
     <Formik
       initialValues={{
         ageCurrent: 30,
@@ -30,6 +31,7 @@ function App() {
 
       }}
       onSubmit={(values) => {
+        setData(values)
         let decimalRentability = values.rentability / 100
         let decimalInflaction = values.inflaction / 100
         let taxLiquid = realTax(decimalRentability, decimalInflaction)
@@ -38,20 +40,24 @@ function App() {
         let initialPatrimonialValue = values.contributionInitial
 
         setCalculated(true)
-        console.log(values)
-        setFvCalculated(
+          setFvCalculated(
           fv(taxLiquid, numberOfPeriods, currentPayments, initialPatrimonialValue)
         )
       }}
     >
-        <Form className='flex flex-col w-fit items-center text-center border-2 border-blue-200 bg-zinc-50 rounded-md gap-3 p-5'>
+        <Form className='flex flex-col w-fit items-center text-center border-2 border-blue-200 bg-zinc-50 dark:text-white dark:bg-zinc-800 rounded-md gap-3 p-5'>
+          <div className='flex justify-end self-start ml-5 rounded-sm p-1 ring-1 ring-slate-400'>
+            <MdOutlineLightMode onClick={switchTheme} className='cursor-pointer' />
+            <RxDividerVertical />
+            <MdModeNight onClick={switchTheme} className='cursor-pointer' />
+          </div>
           <div className="flex justify-between w-full px-5 gap-5">
-            <label htmlFor="ageCurrent" className=''>Idade Atual</label>
+            <label htmlFor="ageCurrent" className=''>Idade atual</label>
             <Field 
               type="number"
               id="ageCurrent"
               name="ageCurrent"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
@@ -60,64 +66,69 @@ function App() {
               type="number"
               id="ageRetired"
               name="ageRetired"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
             <label htmlFor="contributionInitial" className=''>Aporte inicial</label>
             <Field 
               type="number"
+              step="100"
               id="contributionInitial"
               name="contributionInitial"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
             <label htmlFor="contributionRecurrent" className=''>Aporte mensal</label>
             <Field 
               type="number"
+              step="100"
               id="contributionRecurrent"
               name="contributionRecurrent"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
             <label htmlFor="retiredIncome" className=''>Renda esperada após aposentadoria</label>
             <Field 
               type="number"
+              step="100"
               id="retiredIncome"
               name="retiredIncome"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
-            <label htmlFor="rentability" className=''>Retabilidade anual</label>
+            <label htmlFor="rentability" className=''>Rentabilidade anual</label>
             <Field 
               type="number"
               id="rentability"
               name="rentability"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500"
             />
           </div>
           <div className="flex justify-between w-full px-5 gap-5">
-            <label htmlFor="inflaction" className=''>Retabilidade anual</label>
+            <label htmlFor="inflaction" className=''>Inflação esperada anual</label>
             <Field 
               type="number"
               id="inflaction"
               name="inflaction"
-              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400"
+              className="ring-1 ring-blue-300 rounded text-center outline-none focus:shadow-sm focus:shadow-cyan-400 dark:bg-zinc-500 after:content-['%']"
             />
           </div>
           <button 
             type="submit"
-            className="bg-green-500 rounded-md px-4 py-2 text-zinc-900"
+            className="bg-green-500 rounded-md px-4 py-2 text-zinc-900 dark:text-zinc-50"
           >
               Calcular
           </button>
-        {calculated && <span>
-          {`Você terá acumulado: ${formataBRL(fvCalculated)}`}
-        </span>
-        }
+        <span className='h-4'>
+          {
+          calculated && `Até os ${data.ageRetired} anos, você terá acumulado: ${formataBRL(fvCalculated)}`
+          }
+          </span>
+        
         </Form>
     </Formik>
       
